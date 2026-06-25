35ac82343a1e018556ca8c46d2bdec011bcc3b06
dvezinet <didier.vezinet@gmail.com>
2026-06-26 00:20:32 +0200

    [#001] Created fig03_validate_ff

--- articles/RSI_2026_RunawayBremsstrahlungDetection/code/__init__.py
+++ articles/RSI_2026_RunawayBremsstrahlungDetection/code/__init__.py
@@ -1,6 +1,7 @@
 from ._main import main
 from ._fig01_cross import main as fig01_cross
 from ._fig02_dist import main as fig02_dist
+from ._fig03_validate_ff import main as fig03_validate_ff
 from ._fig04_bremsstrahlung import main as fig04_bremsstrahlung
 from ._fig05_tokamak import main as fig05_tokamak
 from ._fig06_responsivities import main as fig06_responsivities
