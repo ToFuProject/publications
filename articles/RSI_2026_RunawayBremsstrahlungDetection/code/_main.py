62b5abc4091c5f250e46ffc828c9749c9bf084f9
dvezinet <didier.vezinet@gmail.com>
2026-06-26 00:17:11 +0200

    [001] Finished renaming

--- articles/RSI_2026_RunawayBremsstrahlungDetection/code/_main.py
+++ articles/RSI_2026_RunawayBremsstrahlungDetection/code/_main.py
@@ -19,9 +19,9 @@ sys.path.pop(0)
 
 from ._fig01_cross import main as fig01
 from ._fig02_dist  import main as fig02
-from ._fig03_tokamak import main as fig03
 from ._fig04_bremsstrahlung import main as fig04
-from ._fig05_responsivities import main as fig05
+from ._fig05_tokamak import main as fig05
+from ._fig06_responsivities import main as fig06
 
 
 # #####################################################
@@ -38,7 +38,7 @@ def main(
     # plot all
     # ---------------
 
-    for fig in [fig01, fig02, fig03, fig04, fig05]:
+    for fig in [fig01, fig02, fig04, fig05, fig06]:
         fig(
             ne_m3=ne_m3,
         )
