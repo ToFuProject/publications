a94baf72cde3b0a4db9d89704ae0ae28e879f93e
dvezinet <dvezinet@cfs.energy>
2026-07-01 19:13:38 +0000

    [#001] Updating fig04

--- articles/RSI_2026_RunawayBremsstrahlungDetection/code/_fig04_bremsstrahlung.py
+++ articles/RSI_2026_RunawayBremsstrahlungDetection/code/_fig04_bremsstrahlung.py
@@ -9,7 +9,7 @@ import datastock as ds
 import tofu as tf
 
 
-from ._fig02_dist import _DDIST
+from . import _load_spect_anis
 from ._savefig import main as savefig
 
 
@@ -104,100 +104,30 @@ def main(
 
     """
 
-    # ------------
-    # d2cross_phi
-    # ------------
-
-    if d2cross_phi is None:
-        d2cross_phi = _PFE_D2CROSS_PHI
-
-    # ------------
-    # inputs
-    # ------------
-
-    if Te_eV is None:
-        Te_eV = _TE
-
-    if jp_fraction_re is None:
-        jp_fraction_re = _JP_FRAC
-
-    if Ekin_max_eV is None:
-        Ekin_max_eV = _EKIN_MAX_EV
-
-    if pnormW is None:
-        pnormW = _PNORMW
-
-    # ------------
-    # ddist
-    # ------------
-
-    ddist = locals()
-    ddist = {
-        kk: vv if ddist.get(kk) is None else ddist[kk]
-        for kk, vv in _DDIST.items()
-        if kk not in ['E_eV', 'theta']
-    }
-    if ne_m3 is not None:
-        ddist['ne_m3'] = ne_m3
-
-    if Ekin_max_eV is not None:
-        ddist['Ekin_max_eV'] = Ekin_max_eV
-
-    if pnormW is not None:
-        ddist['pnormW'] = pnormW
-
-    # shape
-    ddist['Ekin_max_eV'] = np.atleast_1d(ddist['Ekin_max_eV'])[:, None, None]
-    ddist['pnormW'] = np.atleast_1d(ddist['pnormW'])[:, None, None]
-    if ddist['pnormW'].size != ddist['Ekin_max_eV'].size:
-        raise Exception()
-    ddist['Te_eV'] = Te_eV[None, :, None]
-    ddist['jp_fraction_re'] = jp_fraction_re[None, None, :]
-
-    nEkin = ddist['Ekin_max_eV'].shape[0]
-
     # --------------
-    # integrated cross-section
+    # cases
     # --------------
 
-    demiss, ddist, d2cross_phi = tfphysemis.get_xray_thin_integ_dist(
-        # ----------------
-        # cross-section
-        # tabulated d2cross_phi
+    if cases is None:
+        cases = _CASES
+
+    # --------------
+    # demiss
+    # --------------
+
+    demiss, ddist = _load_spect_anis.main(
         d2cross_phi=d2cross_phi,
-        # d2cross_phi computation
-        E_ph_eV=E_ph_eV,
-        E_e0_eV=None,
-        E_e0_eV_npts=None,
-        theta_e0_vsB_npts=None,
-        phi_e0_vsB_npts=None,
-        theta_ph_vsB=None,
-        # inputs
-        Z=None,
-        # hypergeometric parameter
-        ninf=None,
-        source=None,
-        # integration parameters
-        nthetae=None,
-        ndphi=None,
-        # output customization
-        version_cross=None,
-        # save / load
-        save_d2cross_phi=False,
-        # ---------------------
-        # optional responsivity
-        dresponsivity=None,
-        plot_responsivity_integration=None,
-        # -----------
-        # verb
-        debug=False,
-        verb=True,
-        # ----------------
-        # electron distribution
-        **ddist,
+        # dist
+        ne_m3=ne_m3,
+        pnormW=pnormW,
+        Ekin_max_eV=Ekin_max_eV,
+        Te_eV=Te_eV,
+        jp_fraction_re=jp_fraction_re,
+        # emiss
+        E_ph_eV=None,
     )
 
-    units = demiss['emiss']['RE']['emiss']['units']
+    units = demiss['RE']['ff']['units']
     ne = np.unique(ddist['plasma']['ne_m3']['data'])[0]
     jp = np.unique(ddist['plasma']['jp_Am2']['data'])[0]
 
@@ -207,19 +137,11 @@ def main(
     )
 
     # --------------
-    # cases
-    # --------------
-
-    if cases is None:
-        cases = _CASES
-
-    # --------------
     # Elim
     # --------------
 
     shape = ddist['plasma']['Te_eV']['data'].shape
     Elim = np.full(shape, np.nan)
-    theta_Elim = 0
     for ii, ind in enumerate(np.ndindex(shape)):
 
         sli_emiss = ind + (slice(None), 0)
