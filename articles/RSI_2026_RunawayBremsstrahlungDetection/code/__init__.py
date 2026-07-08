b94538f29e3c6d94ec817605097272fe40c6502c
dvezinet <dvezinet@cfs.energy>
2026-07-08 16:32:38 +0000

    [#001] Renaming

--- articles/RSI_2026_RunawayBremsstrahlungDetection/code/__init__.py
+++ articles/RSI_2026_RunawayBremsstrahlungDetection/code/__init__.py
@@ -1,6 +1,7 @@
 from ._main import main
 from ._load_spect import main as load_spect
 from ._load_spect_anis import main as load_spect_anis
+from ._perfs import main as perfs
 from ._fig01_cross import main as fig01_cross
 from ._fig02_dist import main as fig02_dist
 from ._fig02_dist_type import main as fig02_dist_type
@@ -10,4 +11,4 @@ from ._fig04_bremsstrahlung import main as fig04_bremsstrahlung
 from ._fig05_emiss import main as fig05_emiss
 from ._fig06_tokamak import main as fig06_tokamak
 from ._fig07_responsivities import main as fig07_responsivities
-from ._fig08_scores_1keV import main as fig08_scores_1keV
+from ._fig08_perfs_single import main as fig08_perfs_single
