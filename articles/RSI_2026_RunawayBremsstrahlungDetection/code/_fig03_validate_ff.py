e63005c868ae12fae6390600ab65c952c123bae1
dvezinet <dvezinet@cfs.energy>
2026-07-15 18:14:22 +0000

    [#001] Pb identified with fractional abundances => moving on to fig12()

--- articles/RSI_2026_RunawayBremsstrahlungDetection/code/_fig03_validate_ff.py
+++ articles/RSI_2026_RunawayBremsstrahlungDetection/code/_fig03_validate_ff.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7416c8be527a6d8d893d571608bb5829b97cd23a9169ee003e0f59137b65223
-size 7641
+oid sha256:7ba6c595cdb8d8e88a1f3b1b0c726f95b40bdbd358b3122dc9d7b68dd12baecd
+size 7664
